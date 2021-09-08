2744468b60dd292a1c1f570bc74238ca4cdeaa44
Dasha <dasha.kotova.mail@gmail.com>
2021-09-07 20:43:58 -0400

    add the toggle, delete, add functionalities

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -10,9 +10,44 @@ export class TodosComponent implements OnInit {
 
   todos:Todo[];
 
+  inputTodo:string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = [
+
+      {
+        content: 'First todo',
+        completed: false
+      },
+
+      {
+        content: 'Second todo',
+         completed: false
+      }
+    ]
+  }
+
+  toggleDone (id:number) {
+    this.todos.map((v, i ) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    })
+  }
+
+  deleteTodo (id:number) {
+    this.todos = this.todos.filter((v, i ) => i !== id);
+  }
+
+  addTodo () {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = "";
   }
 
 }
